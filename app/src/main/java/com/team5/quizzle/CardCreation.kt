d8ce5048aa6d13b700fcc23f6af31e1a12e3b97a
Oxidiz3 <mecham.porter@gmail.com>
2021-06-15 15:44:31 -0600

    Reworked main_content; Added a create card screen; fixed navigation

--- app/src/main/java/com/team5/quizzle/CardCreation.kt
+++ app/src/main/java/com/team5/quizzle/CardCreation.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +17,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FlashCard.newInstance] factory method to
+ * Use the [CardCreation.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FlashCard : Fragment() {
+class CardCreation : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,8 +37,19 @@ class FlashCard : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flash_card, container, false)
+        val view = inflater.inflate(R.layout.fragment_card_creation, container, false)
+        view.findViewById<Button>(R.id.cardCreationSubmit).setOnClickListener{
+
+            //Make the call to the database here vvvvvvv
+            var question = view.findViewById<TextInputEditText>(R.id.backSideInput).text
+            var answer = view.findViewById<TextInputEditText>(R.id.frontSideInput).text
+
+            Navigation.findNavController(view).navigate(R.id.action_cardCreation_to_mainContent)
+
+        }
+        return view
     }
 
     companion object {
@@ -44,12 +59,12 @@ class FlashCard : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FlashCard.
+         * @return A new instance of fragment CardCreation.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FlashCard().apply {
+            CardCreation().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
