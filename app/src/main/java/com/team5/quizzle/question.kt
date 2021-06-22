c463809bdb243ac8a8154d038103da8f000bb82d
hanchettbm <hanchettbm@gmail.com>
2021-06-22 10:00:40 -0600

    Added Question Layout

--- app/src/main/java/com/team5/quizzle/question.kt
+++ app/src/main/java/com/team5/quizzle/question.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainContent.newInstance] factory method to
+ * Use the [question.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainContent : Fragment() {
+class question : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,13 +34,8 @@ class MainContent : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_main_content, container, false)
-        view.findViewById<FloatingActionButton>(R.id.addFlashCard).setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_mainContent_to_cardCreation)
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_question, container, false)
     }
-
 
     companion object {
         /**
@@ -51,12 +44,12 @@ class MainContent : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainContent.
+         * @return A new instance of fragment question.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainContent().apply {
+            question().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
