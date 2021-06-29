13ed36aa5d22b6b1f6af95388b7f5f465f4982e3
hanchettbm <hanchettbm@gmail.com>
2021-06-29 11:56:13 -0600

    Added Question Layout

--- app/src/main/java/com/team5/quizzle/TestMode.kt
+++ app/src/main/java/com/team5/quizzle/TestMode.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [question.newInstance] factory method to
+ * Use the [TestMode.newInstance] factory method to
  * create an instance of this fragment.
  */
-class question : Fragment() {
+class TestMode : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,22 +34,7 @@ class question : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_question, container, false)
-
-        // study button
-        view.findViewById<Button>(R.id.study_button).setOnClickListener{
-            // TODO: Add different modes to the Flashcard
-            bundle.putString("Mode", "Study")
-            Navigation.findNavController(view).navigate(R.id.action_question_to_flashCard)
-        }
-
-        // test button
-        view.findViewById<Button>(R.id.test_button).setOnClickListener{
-            bundle.putString("Mode", "Test")
-            Navigation.findNavController(view).navigate(R.id.action_question_to_testMode)
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_test_mode, container, false)
     }
 
     companion object {
@@ -61,12 +44,12 @@ class question : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment question.
+         * @return A new instance of fragment TestMode.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            question().apply {
+            TestMode().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
