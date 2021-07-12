1aa51a93f6c95c1db864430e510c701b83ed2e94
Reed <reed.hunsaker@gmail.com>
2021-07-12 09:07:11 -0600

    Added the database to Test mode and updated the CardCreation.kt to name based on the button pressed.

--- app/src/main/java/com/team5/quizzle/TestMode.kt
+++ app/src/main/java/com/team5/quizzle/TestMode.kt
@@ -1,22 +1,34 @@
 package com.team5.quizzle
 
+import android.app.Dialog
 import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.text.Layout
+import android.transition.Slide
+import android.transition.TransitionManager
 import android.util.Log
+import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class TestMode : Fragment() {
     private var word: String? = "loading..."
     private var defintion: String? = null
+    private var userAnswer: String? = null
+    private var index: Int = 1
 
 
     lateinit var wordTextView : TextView
+    lateinit var textInput : TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +40,27 @@ class TestMode : Fragment() {
         savedInstanceState: Bundle?
 
     ): View? {
-        var view = inflater.inflate(R.layout.fragment_test_mode, container, false)
+        val view = inflater.inflate(R.layout.fragment_test_mode, container, false)
+        textInput = view.findViewById<TextInputEditText>(R.id.answerField)
+        textInput.setImeActionLabel("Answer", KeyEvent.KEYCODE_ENTER);
         // Inflate the layout for this fragment
         wordTextView = view.findViewById<TextView>(R.id.textView3)
         wordTextView.text = word
+        view.findViewById<Button>(R.id.btn_answer).setOnClickListener{
+            userAnswer = textInput.text.toString()
+            Log.d(TAG, "Answer: $userAnswer")
+            if (checkAnswer(userAnswer!!)){
+                Log.d(TAG, "User Correct")
+                getVocab()
+
+            }
+            else{
+                Log.d(TAG, "User Incorrect")
+
+            }
+        }
+
+
 
         return view
     }
@@ -40,7 +69,7 @@ class TestMode : Fragment() {
         val db = Firebase.firestore
         val subject = bundle.getString(titleKey)
         Log.d(ContentValues.TAG, "$subject")
-        val docRef = db.collection("Flashcard").document("$subject-1")
+        val docRef = db.collection("Flashcard").document("$subject-$index")
         docRef.get()
             .addOnSuccessListener { document ->
                 if (document != null) {
@@ -49,13 +78,19 @@ class TestMode : Fragment() {
                     wordTextView.text = word
                     defintion = document.data?.get("Definition")?.toString()
                     Log.d(ContentValues.TAG, "Word: $word Definition: $defintion")
+                    index += 1
                 } else {
                     Log.d(ContentValues.TAG, "No such document")
                 }
             }
             .addOnFailureListener { exception ->
                 Log.d(ContentValues.TAG, "get failed with ", exception)
+                index = 1
             }
 
     }
+    private fun checkAnswer(userAnswer: String): Boolean {
+        return userAnswer == defintion
+    }
+
 }
